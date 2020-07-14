56e9d76db997e7f2ec300c915165680dea9a84e3
Lowella369 <lh850210@gmail.com>
2020-07-13 19:06:36 -0500

    modified the file

--- server/server.js
+++ server/server.js
@@ -1,33 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8080;
+var express = require('express');
+var bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+var app = express();
 
-const app = express();
-
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require("./app/models");
-
-db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
-
-app.get("/", function(request, response(){
+app.get('/', function(request, response){
   response.json('Get request received at /');
 });
 
-require("./app/routes/post-routes")(app);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+require('./app/routes/forums-routes.js')(app);
+app.listen(PORT, function(){
+  console.log('Server is running on port ' + PORT);
 });
